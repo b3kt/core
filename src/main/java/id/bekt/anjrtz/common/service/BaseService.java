0cc6719f205aa1a7a254834516995f57d4c6364d
Subekti Cahyowibowo <subekti.cahyo@gmail.com>
2019-12-01 20:52:36 +0800

    Add base service

---
+++ src/main/java/id/bekt/anjrtz/common/service/BaseService.java
@@ -0,0 +1,10 @@
+package id.bekt.anjrtz.common.service;
+
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BaseService {
+    public static Logger logger = LoggerFactory.getLogger("default");
+}
